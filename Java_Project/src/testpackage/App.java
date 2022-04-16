7bf6db3dd83a1f1ad575fce3af3a6cd3a9f14369
paulixe <pchaumont@ensc.fr>
2022-04-16 17:25:51 +0200

    first commit

---
+++ Java_Project/src/testpackage/App.java
@@ -0,0 +1,21 @@
+package testpackage;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x;
+        double a=10;
+        boolean isBig=true;
+        final int B=10;
+        
+        String t="azkdkazdkaz";
+        t=t.toUpperCase();
+
+        String s="123456";
+        s=s.replace("123","WOOWOWO");
+        s.contains("45");
+        Print(s);
+    }
+    public static void Print(String s)
+    {
+        System.out.println(s);
+    }
+}
